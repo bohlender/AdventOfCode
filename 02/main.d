794b03646d92e0851e7cf7960f78ea1611adbabf
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2017-12-02 17:03:15 +0100

    Solved second puzzle of day 02

--- 02/main.d
+++ 02/main.d
@@ -1,16 +1,9 @@
-import std.stdio: writeln;
+import std.stdio: writeln, writefln;
 import std.string: split, splitLines;
 import std.file: readText;
-import std.algorithm: minElement, maxElement;
+import std.algorithm: minElement, maxElement, swap;
 import std.conv: to;
-
-unittest{
-    assert(rowDifference([5,1,9,5]) == 8);
-    assert(rowDifference([7,5,3]) == 4);
-    assert(rowDifference([2,4,6,8]) == 6);
-
-    assert(checksum("5 1 9 5\n7 5 3\n2 4 6 8") == 18);
-}
+import std.exception: enforce;
 
 void main(string[] args) {
     if(args.length != 2){
@@ -18,10 +11,20 @@ void main(string[] args) {
     }else{
         auto input = readText(args[1]);
         auto res1 = checksum(input);
-        //auto res2 = sum2(input.strip);
-        //writefln("First: %s\nSecond: %s", res1, res2);
-        writeln(res1);
+        auto res2 = checksum2(input);
+        writefln("First: %s\nSecond: %s", res1, res2);
     }
+}
+
+//============================================================================
+// Puzzle 1
+//============================================================================
+unittest{
+    assert(rowDifference([5,1,9,5]) == 8);
+    assert(rowDifference([7,5,3]) == 4);
+    assert(rowDifference([2,4,6,8]) == 6);
+
+    assert(checksum("5 1 9 5\n7 5 3\n2 4 6 8") == 18);
 }
 
 auto rowDifference(T)(T[] row){
@@ -34,6 +37,43 @@ uint checksum(in string input){
         const nums = line.split.to!(uint[]);
         const diff = rowDifference(nums);
         res += diff;
+    }
+
+    return res;
+}
+
+//============================================================================
+// Puzzle 2
+//============================================================================
+unittest{
+    assert(rowQuotient([5,9,2,8]) == 4);
+    assert(rowQuotient([9,4,7,3]) == 3);
+    assert(rowQuotient([3,8,6,5]) == 2);
+    
+    assert(checksum2("5 9 2 8\n9 4 7 3\n3 8 6 5") == 9);
+}
+
+auto rowQuotient(T)(in T[] row){
+    for(auto i=0; i<row.length; ++i){
+        for(auto j=i+1; j<row.length; ++j){
+            T big = row[i];
+            T small = row[j];
+            if (row[i]<row[j])
+                swap(big, small);
+            if(big%small == 0)
+                return big/small;
+        }
+    }
+    enforce(false, "Expected a row with evenly divisable values");
+    assert(false);
+}
+
+uint checksum2(in string input){
+    uint res;
+    foreach(line; input.splitLines){
+        const nums = line.split.to!(uint[]);
+        const quot = rowQuotient(nums);
+        res += quot;
     }
 
     return res;
