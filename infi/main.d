3d23dbafc1a4e25bcc819475836976cff410cb49
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2017-12-06 00:15:31 +0100

    Bots move simultaneously

--- infi/main.d
+++ infi/main.d
@@ -1,6 +1,6 @@
 import std.stdio: writeln, writefln;
-import std.file: readText;
-import std.algorithm: startsWith, find, count;
+import std.file: readText, write;
+import std.algorithm: startsWith, find, count, reduce, min, map;
 import std.format: format, formattedRead;
 import std.array: array;
 
@@ -31,12 +31,16 @@ struct Vec2D{
         return Vec2D(this.x + rhs.x, this.y + rhs.y);
     }
 
+    int opCmp(Vec2D other) const {
+        return (x == other.x) ? other.y-y : other.x-x;
+    }
+
     string toString() const {
         return format("(%d, %d)", x, y);
     }
 }
 
-uint collisionCount(string input){
+static uint collisionCount(string input){
     Vec2D[] bots;
     // Parse bot positions
     while(input.startsWith("[")){
@@ -44,24 +48,25 @@ uint collisionCount(string input){
         input.formattedRead!"[%d,%d]"(pos.x, pos.y);
         bots ~= pos;
     }
-    
+
     uint numCollisions;    
-    uint curBot;
     // Process moves
-    for(; input.length>0; curBot = (curBot+1)%bots.length){
-        // Parse next move
-        Vec2D move;
-        const numVars = input.formattedRead!"(%d,%d)"(move.x, move.y);
-        assert(numVars == 2);
+    while(input.length>0){
+        // Bots move simultaneously 
+        foreach(ref bot; bots){
+            // Parse next move
+            Vec2D move;
+            const numVars = input.formattedRead!"(%d,%d)"(move.x, move.y);
+            assert(numVars == 2);
+
+            // Move bot
+            bot = bot + move;
+        }
         
-        // Move bot
-        const succ = bots[curBot] + move;
-        const collision = bots.count(succ) == bots.length-1;
-        //writefln("Robot %s moves with %s\tfrom %s\tto %s\t(%s)", curBot+1, move, bots[curBot], succ, !collision);
-        bots[curBot] = succ;
+        // Only count collision if all bots are at the same location
+        const collision = bots.count(bots[0]) == bots.length;
         if(collision)
             ++numCollisions;
     }
-
     return numCollisions;
 }
