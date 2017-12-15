220e925fcf4a91d3a1b749b2ddacf69eac4809ad
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2017-12-15 15:21:21 +0100

    Solved second puzzle of day 15

--- 15 - Dueling Generators/main.d
+++ 15 - Dueling Generators/main.d
@@ -10,9 +10,8 @@ void main(string[] args) {
         const contents = readText(args[1]);
         auto input = contents.parse;
         auto res1 = 40_000_000.countMatches(input[0], input[1]);
-        //auto res2 = input.regionCount;
-        //writefln("First: %s\nSecond: %s", res1, res2);
-        writeln(res1);
+        auto res2 = 5_000_000.countMatches!("a%4==0", "a%8==0")(input[0], input[1]);
+        writefln("First: %s\nSecond: %s", res1, res2);
     }
 }
 
@@ -25,10 +24,11 @@ static void expect(T1, T2)(T1 expected, T2 actual, in string file = __FILE__, in
 }
 
 //============================================================================
-// Puzzle 1
+// Puzzle 1/2
 //============================================================================
 import std.range: generate, take;
 import std.typecons: Tuple;
+import std.algorithm: filter;
 
 alias Pair(T) = Tuple!(T, T);
 static auto parse(in string contents){
@@ -48,9 +48,9 @@ static auto infGen(in uint factor, in uint prev){
     };
 }
 
-static uint countMatches(in uint numPairs, in uint prevA, in uint prevB){
-    auto gen1 = generate(infGen(16807, prevA));
-    auto gen2 = generate(infGen(48271, prevB));
+static uint countMatches(alias pred1 = "true", alias pred2 = "true")(in uint numPairs, in uint prevA, in uint prevB){
+    auto gen1 = generate(infGen(16807, prevA)).filter!pred1;
+    auto gen2 = generate(infGen(48271, prevB)).filter!pred2;
 
     uint matches;
     foreach(_; 0..numPairs){
@@ -74,4 +74,14 @@ unittest{
     expect([430625591u, 1233683848, 1431495498, 137874439, 285222916], gen2.take(5).array);
 
     expect(1, 5.countMatches(65, 8921));
+}
+
+unittest{
+    auto gen1 = generate(infGen(16807, 65)).filter!(v => v%4 == 0);
+    auto gen2 = generate(infGen(48271, 8921)).filter!(v => v%8 == 0);
+    
+    expect([1352636452u, 1992081072, 530830436, 1980017072, 740335192], gen1.take(5).array);
+    expect([1233683848, 862516352, 1159784568, 1616057672, 412269392], gen2.take(5).array);
+
+    expect(1, 1056.countMatches!("a%4==0", "a%8==0")(65, 8921));
 }
