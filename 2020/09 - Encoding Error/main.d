4a7a01fe7aca92d0d5b47b463a1280d2c26df2e9
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2020-12-09 21:15:27 +0100

    Solve 2020-12-09

---
+++ 2020/09 - Encoding Error/main.d
@@ -0,0 +1,99 @@
+import std.file: readText;
+import std.stdio;
+import std.string;
+import std.algorithm;
+import std.range;
+import std.conv;
+
+void main(string[] args) {
+    if(args.length != 2){
+        writeln("Invalid number of parameters. Expecting one input file.");
+    }else{
+        auto input = args[1].readText.parse;
+        auto res1 = input.sol1(25);
+        auto res2 = input.sol2(25);
+        writefln("First: %s\nSecond: %s", res1, res2);
+    }
+}
+
+auto parse(in string s) {
+    return s.splitter().map!(to!long).array;
+}
+
+//==============================================================================
+// Puzzle 1
+//==============================================================================
+alias Answers = string[];
+
+auto sol1(Range)(Range input, uint preamble) {
+    outer:
+    foreach(window; input.slide(preamble+1)) {
+        auto newNum = window.back;
+        foreach(i; 0..preamble)
+            foreach(j; i..preamble)
+                if(window[i] + window[j] == newNum)
+                    continue outer;
+        return newNum;
+    }
+    assert(0);
+}
+
+//==============================================================================
+// Puzzle 2
+//==============================================================================
+auto sol2(Range)(Range input, uint preamble) {
+    auto badNum = input.sol1(preamble);
+    ulong ubIdx, acc;
+    foreach(lbIdx; input.length.iota) {
+        while(acc < badNum) {
+            acc += input[ubIdx];
+            ubIdx++;
+        }
+
+        auto slice = input[lbIdx..ubIdx];
+        if(slice.sum == badNum) {
+            auto minMax = slice.fold!(min, max);
+            return minMax[0]+minMax[1];
+        }
+        acc -= input[lbIdx];
+    }
+    assert(0);
+}
+
+//==============================================================================
+// Unittests
+//==============================================================================
+unittest {
+    auto content =
+`35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`;
+    auto input = content.parse;
+    expect(127, input.sol1(5));
+    expect(62, input.sol2(5));
+}
+
+static void expect(T1, T2)(T1 expected, T2 actual, in string file = __FILE__, in size_t line = __LINE__) if(is(typeof(expected == actual) == bool)) {
+    import std.format: format;
+    import core.exception: AssertError;
+
+    if(!(expected == actual))
+        throw new AssertError(format("Expected %s but got %s", expected, actual), file, line);
+}
