7ada1037414dd1c5dbd30f2dd3f50aab0aa19ad6
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2021-12-02 00:12:40 +0100

    Wrap tests in deftest

--- 2021/src/day01.clj
+++ 2021/src/day01.clj
@@ -20,7 +20,8 @@
 
 (def sol1 count-incr)
 
-(is (= 7 (sol1 [199 200 208 210 200 207 240 269 260 263])))
+(deftest part1-examples
+  (is (= 7 (sol1 [199 200 208 210 200 207 240 269 260 263]))))
 
 ; ===================
 ; Part 2
@@ -33,15 +34,16 @@
        (map sum)
        count-incr))
 
-(is (= 5 (sol2 [199 200 208 210 200 207 240 269 260 263])))
+(deftest part2-examples
+  (is (= 5 (sol2 [199 200 208 210 200 207 240 269 260 263]))))
 
 ; ===================
 ; Main
 ; ===================
 (defn -main [& args]
-   (if (not= 1 (count args))
-     (println "Invalid number of parameters. Expecting one input file.")
-     (let [[filename] args
-           input (parse-file filename)]
-       (println "First:" (sol1 input))
-       (println "Second:" (sol2 input)))))
+  (if (not= 1 (count args))
+    (println "Invalid number of parameters. Expecting one input file.")
+    (let [[filename] args
+          input (parse-file filename)]
+      (println "First:" (sol1 input))
+      (println "Second:" (sol2 input)))))
