a9708d5cb1e727e749f359a344ef9b7f7abf1079
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2021-12-06 01:32:00 +0100

    Drop coordinate order assumption

--- 2021/src/day05.clj
+++ 2021/src/day05.clj
@@ -5,9 +5,7 @@
 (defn parse-line [s]
   (let [[_ & str-coords] (re-matches #"(\d+),(\d+) -> (\d+),(\d+)" s)
         [x1 y1 x2 y2] (map #(Integer/parseInt %) str-coords)]
-    (if (<= x1 x2)
-      {:src {:x x1 :y y1} :dst {:x x2 :y y2}}
-      {:src {:x x2 :y y2} :dst {:x x1 :y y1}})))
+    {:src {:x x1 :y y1} :dst {:x x2 :y y2}}))
 
 (defn parse [s]
   (map parse-line (string/split-lines s)))
@@ -19,19 +17,17 @@
 ; Part 1
 ; ===================
 (defn get-coords [line]
-  (let [src (:src line)
-        dst (:dst line)
-        min-y (min (:y src) (:y dst))
-        max-y (max (:y src) (:y dst))
-        dx (- (:x dst) (:x src))
-        dy (- (:y dst) (:y src))]
+  (let [[left right]  (sort-by :x [(:src line) (:dst line)])
+        [min-y max-y] (sort (map :y [left right]))
+        dx            (- (:x right) (:x left))
+        dy            (- (:y right) (:y left))]
     (if (= dx 0)
       (for [y (range min-y (+ max-y 1))]
-        {:x (:x src) :y y})
-      (for [x (range (:x src) (+ (:x dst) 1))]
-        (let [x-off (- x (:x src))
+        {:x (:x left) :y y})
+      (for [x (range (:x left) (+ (:x right) 1))]
+        (let [x-off (- x (:x left))
               y-off (* (/ dy dx) x-off)]
-          {:x x :y (+ y-off (:y src))})))))
+          {:x x :y (+ y-off (:y left))})))))
 
 (defn diagonal? [line]
   (and (not= (get-in line [:src :x]) (get-in line [:dst :x]))
@@ -55,8 +51,6 @@
         freqs (frequencies coords)]
    (count (filter #(>= (val %) 2) freqs))))
 
-(def ls (parse "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2"))
-
 (deftest part2-examples
   (is (= 12 (sol2 (parse "0,9 -> 5,9\n8,0 -> 0,8\n9,4 -> 3,4\n2,2 -> 2,1\n7,0 -> 7,4\n6,4 -> 2,0\n0,9 -> 2,9\n3,4 -> 1,4\n0,0 -> 8,8\n5,5 -> 8,2")))))
 
@@ -70,5 +64,3 @@
           input (parse-file filename)]
       (println "First:" (sol1 input))
       (println "Second:" (sol2 input)))))
-
-(-main "inputs/day05.txt")
