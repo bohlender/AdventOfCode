2a64e17fef0459f161bf98f37eaff543d258e0e8
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2021-12-30 01:25:31 +0100

    Switch from 2d array to map

--- 2021/src/day11.clj
+++ 2021/src/day11.clj
@@ -3,12 +3,17 @@
   (:use clojure.set)
   (:use clojure.test))
 
-(defn parse-line [s]
-  (mapv #(Integer/parseInt (str %)) s))
+(defrecord Coord [x y])
+
+(defn enumerate [coll]
+  (map-indexed list coll))
 
 (defn parse [s]
-  "Returns a 2d array. Access via (get-in coll [y x])."
-  (mapv parse-line (string/split-lines s)))
+  (->> (for [[y line] (enumerate (string/split-lines s))
+             [x c]    (enumerate line)
+             :let [energy (Integer/parseInt (str c))]]
+         {(->Coord x y) energy})
+       (apply merge)))
 
 (defn parse-file [filename]
   (parse (slurp filename)))
@@ -16,41 +21,28 @@
 ; ===================
 ; Part 1
 ; ===================
-(defn dims [state]
-  (let [height (count state)
-        width (count (get state 0))]
-    [width height]))
-
-(defn in-bounds? [[x y] state]
-  (let [[w h] (dims state)]
-    (and (<= 0 x (dec w))
-         (<= 0 y (dec h)))))
-
-(defn coords [state]
-  (let [[width height] (dims state)]
-    (for [x (range width)
-          y (range height)]
-      [x y])))
-
-(defn neighbors [[x y]]
+(defn neighbors [coord]
   (for [dx (range -1 2)
         dy (range -1 2)
         :when (not= dx dy 0)]
-    [(+ x dx) (+ y dy)]))
+    (->Coord (+ (:x coord) dx) (+ (:y coord) dy))))
+
+; TODO: Simplify
+(defn update-values
+  "Updates map sequentially"
+  ([m f] (update-values m f (keys m)))
+  ([m f keyseq]
+   (reduce (fn [m k] (update m k f)) m keyseq)))
 
 (defn inc-energy
-  ([state] (inc-energy state (coords state)))
-  ([state coords]
-   (reduce
-     (fn [state [x y]] (update-in state [y x] inc))
-     state
-     coords)))
+  ([state] (update-values state inc))
+  ([state coords] (update-values state inc coords)))
 
-(defn flashes? [state [x y]]
-  (< 9 (get-in state [y x])))
+(defn flashes? [state coord]
+  (< 9 (get state coord)))
 
 (defn flash-coords [state]
-  (filter #(flashes? state %) (coords state)))
+  (filter #(flashes? state %) (keys state)))
 
 (defn propagate-flash
   ([state]
@@ -59,9 +51,7 @@
    (if (empty? flashing)
      [state flashed]
      (let [new-flashed (into flashed flashing)
-           flash-neighbors (filter
-                             #(in-bounds? % state)
-                             (mapcat neighbors flashing))
+           flash-neighbors (filter state (mapcat neighbors flashing))
            new-state (inc-energy state flash-neighbors)
            new-flashing (difference (set (filter #(flashes? new-state %) flash-neighbors)) new-flashed)]
        (recur new-state new-flashed new-flashing)))))
@@ -69,22 +59,13 @@
 (defn step [state]
   (let [energized (inc-energy state)
         [state2 flashed] (propagate-flash energized)]
-    (reduce
-      (fn [state [x y]] (assoc-in state [y x] 0))
-      state2
-      flashed)))
+    (update-values state2 (fn [v] 0) flashed)))
 
 (defn count-flashed [state]
+  (->> state vals (filter zero?) count))
+
+(defn sol1 [state]
   (->> state
-       flatten
-       (filter zero?)
-       count))
-
-(def input "5483143223\n2745854711\n5264556173\n6141336146\n6357385478\n4167524645\n2176841721\n6882881134\n4846848554\n5283751526")
-(def tmp (parse input))
-
-(defn sol1 [init-state]
-  (->> init-state
        (iterate step)
        (take 101)
        (map count-flashed)
@@ -93,8 +74,8 @@
 (deftest part1-examples
   (let [state-5x5 (parse "11111\n19991\n19191\n19991\n11111")
         state-10x10 (parse "5483143223\n2745854711\n5264556173\n6141336146\n6357385478\n4167524645\n2176841721\n6882881134\n4846848554\n5283751526")]
-    (is (= (step state-5x5) [[3 4 5 4 3] [4 0 0 0 4] [5 0 0 0 5] [4 0 0 0 4] [3 4 5 4 3]]))
-    (is (= (step (step state-5x5)) [[4 5 6 5 4] [5 1 1 1 5] [6 1 1 1 6] [5 1 1 1 5] [4 5 6 5 4]]))
+    (is (= (step state-5x5) (parse "34543\n40004\n50005\n40004\n34543")))
+    (is (= (step (step state-5x5)) (parse "45654\n51115\n61116\n51115\n45654")))
     (is (= (sol1 state-10x10) 1656))))
 
 ; ===================
@@ -102,13 +83,7 @@
 ; ===================
 
 (defn all-flash? [state]
-  (->> state
-       flatten
-       (every? zero?)))
-
-(defn enumerate [coll]
-  (->> coll
-       (map-indexed list)))
+  (->> state vals (every? zero?)))
 
 (defn sol2 [init-state]
   (->> init-state
@@ -118,7 +93,7 @@
        first
        first))
 
-(deftest part1-examples
+(deftest part2-examples
   (let [state-10x10 (parse "5483143223\n2745854711\n5264556173\n6141336146\n6357385478\n4167524645\n2176841721\n6882881134\n4846848554\n5283751526")]
     (is (= (sol2 state-10x10) 195))))
 
