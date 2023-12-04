af8b1940fa25e6c4a7077ed36d837d418f689725
Dimitri Bohlender <bohlender@embedded.rwth-aachen.de>
2023-12-04 23:03:06 +0100

    Use transducer

--- 2023/src/day04.clj
+++ 2023/src/day04.clj
@@ -40,9 +40,7 @@
       (->> num-matches dec (pow 2) int))))
 
 (defn sol1 [input]
-  (->> input
-       (map score)
-       (reduce +)))
+  (transduce (map score) + input))
 
 (deftest part1-examples
   (let [input (parse "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11")]
@@ -52,6 +50,7 @@
 ; Part 2
 ; ==============================================================================
 (defn win-cards [cards]
+  """Returns how many instances of each card one has after winning new ones."""
   (let [init-card-counts (into {} (for [card cards] {(:id card) 1}))]
     (reduce (fn [card-counts card]
               (let [cards-till-end (- (count cards) (:id card))
